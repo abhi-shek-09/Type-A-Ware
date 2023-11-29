577deccb6bec70d71df65877f9976eb8b9d48a8d
unknown <abhisheklfps@gmail.com>
2023-11-29 12:13:04 +0530

    extract-and-clean

---
+++ news_data.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import codecs
+
+def remove_unicode_escapes(text):
+    if text is not None:
+        text = codecs.escape_decode(text.encode('utf-8'))[0].decode('utf-8')
+    return text
+
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+api_key = config['api_key']
+country = "us"
+category = "business"
+url = "https://newsapi.org/v2/top-headlines"
+params = {
+    "country": country,
+    "category": category,
+    "apiKey": api_key
+}
+
+response = requests.get(url, params=params)
+if response.status_code == 200:
+    content = response.json()
+    articles = content['articles']
+    descriptions = []
+    index = 0
+    for article in articles:
+        descriptions.append(article['description'])
+        index += 1
+    
+    cleaned_descriptions = [description for description in descriptions if description is not None and description != "null"]
+    cleaned_descriptions = [description for description in cleaned_descriptions if "<a href" not in description]
+    cleaned_descriptions = [remove_unicode_escapes(description) for description in cleaned_descriptions]
+
+    with open('news_data.json', 'w') as file:
+        json.dump(cleaned_descriptions, file, indent=4)
+
+else:
+    print(f"Error: {response.status_code}")
+    print(response.text)
+
